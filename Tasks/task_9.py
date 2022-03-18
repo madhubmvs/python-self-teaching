46300e71bd2ff29516e2ffa21a918ef283d64384
Prudhvi <madhubmvs@gmail.com>
2022-03-18 22:44:25 +0100

    Committing task 9 - Pallindrome

---
+++ Tasks/task_9.py
@@ -0,0 +1,9 @@
+# Pallindrome
+
+input_string = input("Enter the word: ")
+if  input_string == input_string[::-1]:
+    print("The word is a pallindrome")
+else:
+    print("The word is not a pallindrome")
+
+
